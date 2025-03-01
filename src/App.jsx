a7afb6fe4f505edce455461ef568a87d49b146b0
John Paul Varde <johnpaulvarde13@gmail.com>
2025-03-01 19:51:07 +0800

    1. Adding a function for random array 2. Make a dynamic to display the list of array 3. Make a dynamic img

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+
+import ReactImg from './assets/react-core-concepts.png';
+
+
+const DescriptionArray = ['Paul', 'Varde', 'React Developer'];
+
+function genRandomInt(max) { 
+  return (
+    Math.floor(Math.random() * (max + 1))
+  );
+}
+
+
 function App() {
+
+  const Description = DescriptionArray[genRandomInt(2)];
+
   return (
     <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={ReactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {Description} Fundamental React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
