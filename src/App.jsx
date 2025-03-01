0e55f0800b4280f85c3756c61bedf20efc0d181d
John Paul Varde <johnpaulvarde13@gmail.com>
2025-03-01 20:18:24 +0800

    Create a reusable props and exploring alternative props syntaxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import ReactImg from './assets/react-core-concepts.png';
+import componentImg from './assets/components.png';
+import {CORE_CONCEPTS} from './data';
 
 
 const DescriptionArray = ['Paul', 'Varde', 'React Developer'];
@@ -10,6 +12,15 @@ function genRandomInt(max) {
   );
 }
 
+function CoreConcepts({image, title, description}) {
+  return ( 
+    <li>
+      <img src={image} alt="" />
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </li>
+  );
+}
 
 function App() {
 
@@ -26,6 +37,15 @@ function App() {
         </p>
       </header>
       <main>
+        <section id="core-concepts">
+          <ul>
+            {/* Orter way to create a dynamic set props */}
+            <CoreConcepts image={CORE_CONCEPTS[0].image} title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description}/>
+            {/* short for create a dynamic set props */}
+            <CoreConcepts {... CORE_CONCEPTS[1]}/>
+            <CoreConcepts {... CORE_CONCEPTS[2]}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
