3997475b736cd68634003683c68c48edb7f3263b
John Paul Varde <johnpaulvarde13@gmail.com>
2025-03-21 19:20:30 +0800

    CSS Styling & Dynamic Styling

--- src/Components/TabButton.jsx
+++ src/Components/TabButton.jsx
@@ -1,9 +1,9 @@
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, isSelected}) {
   console.log('TAB COMPONENT EXECUTING')
 
     return (
        <li>
-         <button onClick={onSelect}>{children}</button>
+         <button className={isSelected ? 'active' : null} onClick={onSelect}>{children}</button>
        </li>
    
     );
